883ae60e8f8504eccfeb2de8c26b8951eddd943a
shaikh88 <shaikhmanik4@gmail.com>
2024-07-06 18:15:36 +0530

    added null check

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -39,7 +39,7 @@ const ExperienceDetails = ({ experience }) => {
   return (
     <div className="mt-5">
       <ul className="max-w-7xl list-none space-y-8 border-4 lg:border-8 rounded-xl lg:rounded-3xl p-6">
-        {experience.details.map((detail, index) => (
+        {experience?.details?.map((detail, index) => (
           <li
             key={`experience-detail-${index}`}
             className="text-slate-500 font-semibold text-[10px] xs:text-[14px] md:text-[18px] lg:text-[22px] xl:text-[28px] lg:leading-[30px]"
